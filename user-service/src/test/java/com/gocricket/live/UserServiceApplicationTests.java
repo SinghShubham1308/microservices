efa5fb29d581ac3de06929e4b0830c776f1cb025
SinghShubham1308 <shubhusingh1308@gmail.com>
2025-07-15 23:15:39 +0530

    user-serivice created

---
+++ user-service/src/test/java/com/gocricket/live/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gocricket.live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
